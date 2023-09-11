dd4636e75bb49c6e496d77a27eeff81ebb662b10
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-09-11 16:59:43 +0900

    #1 - 프로젝트 생성

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kotlin-board
+
 코틀린으로 간단한 CRUD 구현하기
---
+++ src/test/kotlin/project/kotlin_board/KotlinBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package project.kotlin_board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
