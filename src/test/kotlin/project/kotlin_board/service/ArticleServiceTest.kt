dc70d3ad423a66a24759559fd717f586067e3e57
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-09-14 16:05:33 +0900

    #6 - 테스트 코드 수정
    오타 수정과 findByIdOrNull 이 mockito 에 없어서
    이 함수를 직접적으로 비교 할 수 없어 findById 를 사용해
    Optional 을 이용해 테스트 작성

--- src/test/kotlin/project/kotlin_board/service/ArticleServiceTest.kt
+++ src/test/kotlin/project/kotlin_board/service/ArticleServiceTest.kt
@@ -9,7 +9,6 @@ import org.mockito.BDDMockito.*
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import project.kotlin_board.dto.request.ArticleDeleteRequest
 import project.kotlin_board.dto.request.ArticleRequest
@@ -19,6 +18,7 @@ import project.kotlin_board.model.entity.Article
 import project.kotlin_board.model.entity.User
 import project.kotlin_board.model.repository.ArticleRepository
 import project.kotlin_board.model.repository.UserRepository
+import java.util.*
 
 @DisplayName("게시글 서비스 테스트")
 @ExtendWith(MockitoExtension::class)
@@ -29,10 +29,14 @@ class ArticleServiceTest {
 
     @Mock
     lateinit var articleRepository: ArticleRepository
+
     @Mock
     lateinit var userRepository: UserRepository
+
     @Mock
     lateinit var encoder: BCryptPasswordEncoder
+
+    //TODO : findByIdOrNull 이 mockito 에 없어서 이 함수를 직접적으로 비교 할 수 없어 findById 를 사용해 Optional 을 이용해 테스트 작성
 
     @DisplayName("게시글 생성 요청 - 정상 동작")
     @Test
@@ -50,7 +54,7 @@ class ArticleServiceTest {
         val result = sut.create(request)
 
         //then
-        then(sut).should().create(request)
+        then(articleRepository).should().save(any(Article::class.java))
         assertThat(result.title).isEqualTo(request.title)
         assertThat(result.content).isEqualTo(request.content)
     }
@@ -100,7 +104,7 @@ class ArticleServiceTest {
 
         given(userRepository.findByEmail(email)).willReturn(user)
         given(encoder.matches(request.password, user.password)).willReturn(true)
-        given(articleRepository.findByIdOrNull(any())).willReturn(article)
+        given(articleRepository.findById(any())).willReturn(Optional.of(article))
         given(articleRepository.save(any())).willReturn(article)
 
         //when
@@ -123,14 +127,14 @@ class ArticleServiceTest {
 
         given(userRepository.findByEmail(email)).willReturn(user)
         given(encoder.matches(request.password, user.password)).willReturn(true)
-        given(articleRepository.findByIdOrNull(any())).willReturn(null)
+        given(articleRepository.findById(any())).willReturn(Optional.empty())
 
         //when & then
 
         assertThatThrownBy { sut.update(articleId, request) }
             .isInstanceOf(BoardApplicationException::class.java)
             .extracting("errorCode")
-            .isEqualTo(ErrorCode.INVALID_PASSWORD)
+            .isEqualTo(ErrorCode.ARTICLE_NOT_FOUND)
     }
 
     @DisplayName("게시글 작성자가 아닌 사람이 수정 요청하면, 예외를 반환한다.")
@@ -145,7 +149,7 @@ class ArticleServiceTest {
 
         given(userRepository.findByEmail(differEmail)).willReturn(differUser)
         given(encoder.matches(request.password, differUser.password)).willReturn(true)
-        given(articleRepository.findByIdOrNull(any())).willReturn(article)
+        given(articleRepository.findById(any())).willReturn(Optional.of(article))
 
         //when & then
         assertThatThrownBy { sut.update(articleId, request) }
@@ -166,7 +170,7 @@ class ArticleServiceTest {
 
         given(userRepository.findByEmail(email)).willReturn(user)
         given(encoder.matches(request.password, user.password)).willReturn(true)
-        given(articleRepository.findByIdOrNull(any())).willReturn(article)
+        given(articleRepository.findById(any())).willReturn(Optional.of(article))
         willDoNothing().given(articleRepository).delete(article)
 
         //when
@@ -187,7 +191,7 @@ class ArticleServiceTest {
 
         given(userRepository.findByEmail(email)).willReturn(user)
         given(encoder.matches(request.password, user.password)).willReturn(true)
-        given(articleRepository.findByIdOrNull(any())).willReturn(null)
+        given(articleRepository.findById(any())).willReturn(Optional.empty())
 
         //when & then
         assertThatThrownBy { sut.delete(articleId, request) }
@@ -202,14 +206,14 @@ class ArticleServiceTest {
     fun givenDifferUser_whenDeletingArticle_thenReturnException() {
         //given
         val differEmail = "eamilDifferent@email.com"
-        val request = createDeleteArticle()
+        val request = createDeleteArticle(differEmail)
         val differUser = createUser(2L, differEmail)
         val articleId = 1L
         val article = createArticle(articleId)
 
         given(userRepository.findByEmail(differEmail)).willReturn(differUser)
         given(encoder.matches(request.password, differUser.password)).willReturn(true)
-        given(articleRepository.findByIdOrNull(any())).willReturn(article)
+        given(articleRepository.findById(any())).willReturn(Optional.of(article))
 
         //when & then
         assertThatThrownBy { sut.delete(articleId, request) }
@@ -218,7 +222,7 @@ class ArticleServiceTest {
             .isEqualTo(ErrorCode.INVALID_PERMISSION)
     }
 
-    private fun createArticle(articleId : Long) : Article{
+    private fun createArticle(articleId: Long): Article {
         return Article(articleId, "title", "content", createUser())
     }
 
@@ -230,7 +234,7 @@ class ArticleServiceTest {
         return createArticleRequest(email, "password")
     }
 
-    private fun createArticleRequest(email: String, password: String): ArticleRequest{
+    private fun createArticleRequest(email: String, password: String): ArticleRequest {
         return ArticleRequest(email, password, "title", "content")
     }
 
@@ -238,15 +242,19 @@ class ArticleServiceTest {
         return createUser(1L, email)
     }
 
-    private fun createUser(userId: Long, email: String) : User{
-         return createUser(userId, email, "password")
+    private fun createUser(userId: Long, email: String): User {
+        return createUser(userId, email, "password")
     }
 
     private fun createUser(userId: Long, email: String, password: String): User {
         return User(userId, email, "username", password)
     }
 
-    private fun createDeleteArticle() : ArticleDeleteRequest{
-        return ArticleDeleteRequest("email@email.com", "password")
+    private fun createDeleteArticle(): ArticleDeleteRequest {
+        return createDeleteArticle("email@email.com")
+    }
+
+    private fun createDeleteArticle(email: String): ArticleDeleteRequest {
+        return ArticleDeleteRequest(email, "password")
     }
 }
