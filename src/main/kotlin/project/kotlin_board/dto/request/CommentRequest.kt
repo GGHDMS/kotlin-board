cbd974e40e0ba1747155ad981f5647cda69b6c8a
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-10-09 20:57:52 +0900

    #28 - jwt 기능 추가로 인한 dto 변경
    이전 request 에 있던 불필요한 부분 제거함

--- src/main/kotlin/project/kotlin_board/dto/request/CommentRequest.kt
+++ src/main/kotlin/project/kotlin_board/dto/request/CommentRequest.kt
@@ -3,12 +3,6 @@ package project.kotlin_board.dto.request
 import javax.validation.constraints.NotBlank
 
 data class CommentRequest(
-    @field:NotBlank(message = "Email cannot be blank")
-    val email: String,
-
-    @field:NotBlank(message = "Password cannot be blank")
-    val password: String,
-
     @field:NotBlank(message = "Content cannot be blank or empty")
     val content: String
 )
--- src/main/kotlin/project/kotlin_board/dto/request/SignInRequest.kt
+++ src/main/kotlin/project/kotlin_board/dto/request/SignInRequest.kt
@@ -1,12 +1,13 @@
 package project.kotlin_board.dto.request
 
+import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 
-class ArticleDeleteRequest(
+data class SignInRequest(
+    @field:Email(message = "Please provide a valid email address")
     @field:NotBlank(message = "Email cannot be blank")
     val email: String,
 
     @field:NotBlank(message = "Password cannot be blank")
-    val password: String
+    val password: String,
 )
-
