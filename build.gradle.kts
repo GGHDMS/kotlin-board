435be8c5fa051328d8516333907849500de7fa95
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-11-21 14:07:31 +0900

    #52 - `QueryDsl` 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,15 @@ plugins {
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
+
+    kotlin("kapt") version "1.8.22"
+    idea
 }
 
 group = "project"
 version = "0.0.1-SNAPSHOT"
+
+val queryDslVersion = "5.0.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -34,6 +39,9 @@ dependencies {
     // JWT
     implementation("io.jsonwebtoken:jjwt:0.9.1")
 
+    implementation("com.querydsl:querydsl-jpa:5.0.0")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+
     testImplementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
@@ -52,4 +60,12 @@ tasks.withType<Test> {
 
 tasks.jar {
     enabled = false
+}
+
+idea {
+    module {
+        val kaptMain = file("build/generated/source/kapt/main")
+        sourceDirs.add(kaptMain)
+        generatedSourceDirs.add(kaptMain)
+    }
 }
